ad47afe545092babd76748cd9846d3702d400cc2
igorsalesleallima <igorsalesleallima@gmail.com>
2026-04-10 21:30:22 -0300

    atualização

---
+++ projeto ClimasX/templates/js/code.js
@@ -0,0 +1,11 @@
+function console(){
+    var a = [];
+    a[0] = "funcionando";
+    a[1] = "funcionando";
+    a[2] = "funcionando";
+    a[3] = "funcionando";
+    a[4] = "funcionando";
+
+    console.log(a);
+}
+console()
